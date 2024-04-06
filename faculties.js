9075a17b367321a0d8485a08ebdee7be3e09dba8
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-04-06 12:44:11 +0530

    Add files via upload

---
+++ faculties.js
@@ -0,0 +1,32 @@
+[
+    {
+        "name" : "Sam",
+        "phone" : 9999888767,
+        "skills" : ["DBMS", "C++", "Python"],
+        "designation" : "Professor"
+    },
+    {
+        "name" : "Jack",
+        "phone" : 9999000767,
+        "skills" : ["C++", "Python", "OS", "Networking"],
+        "designation" : "HOD"
+    },
+    {
+        "name" : "John",
+        "phone" : 9967188767,
+        "skills" : ["DBMS", "Python", "DSA"],
+        "designation" : "Professor"
+    },
+    {
+        "name" : "Nick",
+        "phone" : 9999888000,
+        "skills" : ["DBMS", "C++", "Java"],
+        "designation" : "Professor"
+    },
+    {
+        "name" : "Tom",
+        "phone" : 9009888767,
+        "skills" : ["DBMS", "C++", "Python", "Java", "Mongo"],
+        "designation" : "HOD"
+    },
+]
---
+++ students.js
@@ -0,0 +1,30 @@
+[
+    {
+        "name" : "Max",
+        "year" : 4,
+        "phone" : 7899844444,
+        "course" : "BCA",
+        "batch" : 2022,
+    },
+    {
+        "name" : "Nick",
+        "year" : 2,
+        "phone" : 7899844004,
+        "course" : "BCA",
+        "batch" : 2022,
+    },
+    {
+        "name" : "Adam",
+        "year" : 1,
+        "phone" : 7800844444,
+        "course" : "Btech",
+        "batch" : 2019,
+    },
+    {
+        "name" : "Naman",
+        "year" : 2,
+        "phone" : 7897777444,
+        "course" : "Btech",
+        "batch" : 2020,
+    },
+]
